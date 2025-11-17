4191b116fe80f9e4232450b78b22e6deaf51a976
Anikcse18 <istiakjavedanik@gmail.com>
2025-11-17 22:47:42 +0100

    services added

--- my_robot_controller/turtle_controller.py
+++ my_robot_controller/turtle_controller.py
@@ -3,11 +3,13 @@ import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose
-
+from turtlesim.srv import SetPen
+from functools import partial
 
 class TurtleControllerNode(Node):
     def __init__(self):
         super().__init__('turtle_controller') #node name
+        self.previous_x = 0.0;
 
         self.cmd_val_publisher = self.create_publisher(
             Twist,"/turtle1/cmd_vel",10) 
@@ -21,24 +23,53 @@ class TurtleControllerNode(Node):
         cmd =Twist()
 
         if pose.x >=9.0  or pose.x <2 or pose.y >9.0 or pose.y <2:
-            cmd.linear.x = 1.0;
-            cmd.linear.y = 1.0;
-            cmd.angular.z = 0.9;
+            cmd.linear.x = 1.0
+            cmd.linear.y = 1.0
+            cmd.angular.z = 0.9
         else:
             cmd.linear.x = 5.0
             cmd.angular.z = 0.0
         self.cmd_val_publisher.publish(cmd)
 
+        if pose.x>5.5 and self.previous_x <=5.5:
+             self.previous_x = pose.x    
+             self.get_logger().info('Changing Pen Colour to Red')
+             self.call_set_pen_colour(255,0,0,3,0)
+
+        elif pose.x <= 5.5 and self.previous_x > 5.5:
+            self.previous_x = pose.x
+            self.get_logger().info('Changing Pen Colour to Red')
+            self.call_set_pen_colour(0,0,255,3,0)
 
 
+    def call_set_pen_colour(self,r,g,b,width,off):
+            client = self.create_client (SetPen, '/turtle1/set_pen')
+            while not client.wait_for_service(timeout_sec = 1.0):
+                 self.get_logger().warm("Waiting for Service......")
 
-        # def send_velocity(self):
-        #     msg =Twist()
-        #     msg.linear.x = 2.0
-        #     msg.angular.z = 1.0
-        #     self.cmd_vel_pub.publish(msg)
+            request = SetPen.Request()
+
+            request.r = r
+            request.g = g
+            request.b = b
+            request.width = width
+            request.off = off
+
+            future = client.call_async(request)
+
+            future.add_done_callback(self.callback_set_pen)           
 
 
+    def callback_set_pen(self,future):
+        try:
+            response = future.result()
+            self.get_logger().info("SetPen service call successful....")
+
+        except Exception as e:
+             self.get_logger().error(
+                 'Service call failed %r' % (e,))
+
+    
 
 def main(args=None):
 
